a9cea3b7adb43c3df7915f318e6adb07891fc780
Felipe Brandão <felipe.brandao98@icloud.com>
2021-10-13 16:19:43 -0300

    refactor to components style

---
+++ components/header/header.php
@@ -0,0 +1,30 @@
+<head>
+  <link rel="stylesheet" href="components/header/styles/header.css">
+  <link rel="stylesheet" href="components/header/styles/menu.css">
+  <link rel="stylesheet" href="components/header/styles/inputdarkmode.css">
+</head>
+<body>
+  <header>
+    <img alt="Alves E Mattos">
+
+    <menu>
+      <a href="" class="selected">HOME</a>
+      <a href="">INSTITUCIONAL</a>
+      <a href="">NEWSLETTER</a>
+      <a href="">CONTATO</a>
+    </menu>
+    
+    <div class="toggle">
+      <span>
+        🌞
+      </span>
+      <input id="switch" type="checkbox" name="theme">
+      <label for="switch">Toggle</label>
+      <span>
+        🌚
+      </span>
+    </div>
+  </header>
+  
+  <script src="components/header/scripts/nightmode.js"></script>
+</body>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+
+  <link rel="stylesheet" href="styles/global.css">
+
+
+  <title>Alves E Mattos - Advogados Associados</title>
+</head>
+<body>
+  <?php
+    include_once 'components/header/header.php';
+  ?>
+</body>
+</html>
