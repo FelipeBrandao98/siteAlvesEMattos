8efd049ae19ff9e183b735246825d12c9ac90d8d
Felipe Brandão <felipe.brandao98@icloud.com>
2021-10-28 11:56:00 -0300

    add Slogan

---
+++ components/slogan/slogan.php
@@ -0,0 +1,9 @@
+<head>
+  <link rel="stylesheet" href="components/slogan/styles/slogan.css">
+</head>
+
+<body>
+  <div class="slogan">
+    <h1>Ética, independência,<br> dedicação e confiança<br> são os valores que <br>norteiam nosso trabalho</h1>
+  </div>
+</body>
